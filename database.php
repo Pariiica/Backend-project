3e7c24e085484871e16fad329d5776edd27c7d59
parica <parisanaderi456@gmail.com>
2023-11-09 12:03:56 +0330

    mysqli object oriented

--- database.php
+++ database.php
@@ -2,9 +2,8 @@
 $servername = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = 'connect-db';
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
