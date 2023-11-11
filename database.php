adb29cf57fe8df3b2a6490a663a923be53255c85
parica <parisanaderi456@gmail.com>
2023-11-11 16:45:40 +0330

    db updated

--- database.php
+++ database.php
@@ -15,12 +15,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = mysqli_real_escape_string($conn, $_REQUEST['username']);
     $password =  mysqli_real_escape_string($conn, $_REQUEST['password']);
     $email = isset($email) ? mysqli_real_escape_string($conn, $_REQUEST['email']) : null;
-    
     $sql = "INSERT INTO info (username, email, password)   
     VALUES ('$username', '$email', '$password')";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
+        header("Location: index.php");
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
